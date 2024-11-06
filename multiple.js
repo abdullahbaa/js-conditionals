a5f5e0eec671ae4c954b8c9662312bed7eb44b20
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-11-06 23:10:29 +0600

    added nasted loop part

---
+++ multiple.js
@@ -0,0 +1,30 @@
+const salary =25000;
+const isBCS = true;
+const height =61;
+const hascar =false;
+
+// if (salary > 20000 && height > 66 ){
+//     console.log("su patro");
+
+// }
+// else{
+//     console.log("onno patro khuji")
+// }
+
+// if(salary > 25000 || height > 72 || isBCS == true){
+//     console.log("esho baba kobul");
+
+// }
+// else{
+//     console.log("bhag tui mokbul")
+// }
+
+// .......................Complex Condition........
+
+if((salary > 25000 && hascar ==true) || isBCS == true){
+    console.log("tomar 14 gushti raji");
+
+}
+else{
+    console.log("bhag tui hala")
+}
---
+++ nasted.js
@@ -0,0 +1,19 @@
+const money =-20;
+if (money > 300)
+{
+    console.log("bro you are rich");
+}
+else{
+    if(money > 100){
+        console.log(" tui gorib hala");
+    }
+    else{
+        if(money > 0)
+        {
+            console.log("tui fokinni hala");
+        }
+     else{
+        console.log("you are out")
+     }
+    }
+}
