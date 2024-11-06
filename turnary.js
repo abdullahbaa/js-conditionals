a3c232b243119c63741986f7f4157032c18a6bec
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-11-06 23:41:07 +0600

    added todays codes

--- turnary.js
+++ turnary.js
@@ -20,14 +20,14 @@ const age =20;
 
 let price =500;
 const isLeader =false;
-if(isLeader == true)
-{
-    price=0;
-}
-else{
-    price = price + 100;
-}
-console.log(price);
+// if(isLeader == true)
+// {
+//     price=0;
+// }
+// else{
+//     price = price + 100;
+// }
+// console.log(price);
 
 price = isLeader == true ? 0: price + 100;
 
