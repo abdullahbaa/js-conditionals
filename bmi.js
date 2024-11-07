edf58ccd4ed57649ca8d9d5c7ecee647cf55da03
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-11-07 12:25:35 +0600

    added task2

---
+++ bmi.js
@@ -0,0 +1,22 @@
+// let bmi = 23.00;
+let bmi
+let weight = 95;
+let height = 690;
+
+height= 590 / 100
+bmi = 70 / (1.7 * 1.7)
+bmi = 70 / 2.89
+bmi = 24.22
+
+if(bmi < 18.5){
+    console.log('underweight');
+}
+else if(bmi >= 18.5 && bmi<= 24.9){
+    console.log('you are normal');
+}
+else if(bmi >=25 && bmi<= 29.9){
+    console.log('you are overweight');
+}
+else{
+    console.log(' you are obese');
+}
